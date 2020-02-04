be53b019a3a5f91720f409ec49cfdc483160fa28
Alex Eliacik <57728302+alexeliacik@users.noreply.github.com>
2020-02-03 19:26:45 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { createStore } from "redux"
 import "./index.css"
@@ -13,7 +13,9 @@ const store = createStore(reducer)
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+     <HashRouter>
       <App />
+     </HashRouter>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
