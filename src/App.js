fbc3f35655c88c6209da9b96e389a76999300b65
Alex Eliacik <57728302+alexeliacik@users.noreply.github.com>
2020-02-11 17:31:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import { Route, Switch } from "react-router-dom"
 import Navigation from "./components/Navigation"
 import Colors from "./components/Colors"
 import SelectedColors from "./components/SelectedColors"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  *, *::after, *::before {
+    padding:0;
+    margin:0;
+    box-sizing: inherit;
+  }
+  body {
+    box-sizing: border-box;
+  }
+`
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyle />
       <Navigation />
       <Switch>
         <Route path='/selected-colors' component={SelectedColors} />
